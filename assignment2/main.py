ee0c79373765e9355ca36011d0ddbae48d5926b6
somal <somal1996@rambler.ru>
2017-02-06 20:26:39 +0300

    Add clear

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,6 +1,12 @@
 import os
 import webapp2
 from google.appengine.ext.webapp import template
+from google.appengine.ext import ndb
+
+
+class Greeting(ndb.Model):
+    content = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -13,6 +19,14 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('Hello, World!')
 
 
+class Clear(webapp2.RequestHandler):
+    def post(self):
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key()).fetch(keys_only=True)
+        ndb.delete_multi(greetings_query)
+        self.redirect('/')
+
+
 def handle_404(request, response, exception):
     """Return a custom 404 error."""
     response.write('Sorry, nothing at this URL.')
@@ -21,5 +35,6 @@ def handle_404(request, response, exception):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/clear', Clear),
 ], debug=True)
 application.error_handlers[404] = handle_404
