db2a28a3c2ab6631152102f1e1d45629e75ba896
somal <somal1996@rambler.ru>
2017-02-06 20:03:07 +0300

    Delete garbage

--- assignment2/main.py
+++ assignment2/main.py
@@ -13,13 +13,6 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('Hello, World!')
 
 
-class JS(webapp2.RequestHandler):
-    def get(self):
-        # path = os.path.join(os.path.dirname(__file__), 'survey.js')
-        with open("survey.js", "r") as f:
-            self.response.write(f.read())
-
-
 def handle_404(request, response, exception):
     """Return a custom 404 error."""
     response.write('Sorry, nothing at this URL.')
@@ -28,6 +21,5 @@ def handle_404(request, response, exception):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/survery.js', JS),
 ], debug=True)
 application.error_handlers[404] = handle_404
