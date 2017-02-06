3a7436075fc570dcbacfedba0c670e723e9e3738
somal <somal1996@rambler.ru>
2017-02-06 19:58:52 +0300

    Refactoring

--- assignment2/survey/main.py
+++ assignment2/survey/main.py
@@ -7,16 +7,17 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         """Return a friendly HTTP greeting."""
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-	self.response.out.write(template.render(path, {} ))
+        self.response.out.write(template.render(path, {}))
 
-	#self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.write('Hello, World!')
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.write('Hello, World!')
+
 
 class JS(webapp2.RequestHandler):
     def get(self):
-        #path = os.path.join(os.path.dirname(__file__), 'survey.js')
-	with open("survey.js", "r") as f:
-          self.response.write(f.read())
+        # path = os.path.join(os.path.dirname(__file__), 'survey.js')
+        with open("survey.js", "r") as f:
+            self.response.write(f.read())
 
 
 def handle_404(request, response, exception):
