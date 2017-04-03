c27a8dee36e978c96a33837bbb2538a09de8108c
somal <somal1996@rambler.ru>
2017-04-04 02:11:07 +0300

    Add data saving to store

--- assignment5/index/views.py
+++ assignment5/index/views.py
@@ -1,10 +1,18 @@
+import json
+
 from django.shortcuts import render
+
+from .models import Voice
 
 
 def main(request):
     # return render(request, 'static/index.html')
     if request.method == 'POST':
-        print(request.POST)
+        data = request.POST['data']
+        data = json.loads(data)
+        voice_id = Voice.get_max_id() + 1
+        for k, v in data.items():
+            Voice.objects.create(voice_id=voice_id, key=k, value=v)
     return render(request, 'index.html')
 
 
