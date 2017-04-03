c5ebb128d7e0033c107ecb78e7d8617d91696bfd
somal <somal1996@rambler.ru>
2017-04-04 02:02:02 +0300

    Update models

--- assignment5/index/models.py
+++ assignment5/index/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Voice(models.Model):
+    voice_id = models.IntegerField()
     key = models.CharField(max_length=200)
     value = models.CharField(max_length=100)
+
+    def get_max_id(self):
+        return self.objects.all().order_by('-voice_id').first()
